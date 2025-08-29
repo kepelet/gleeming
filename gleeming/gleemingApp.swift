8a2bf9ab2c67c6a1ffe0a200c3b96f142c85cbd6
ervan <ervanroot@gmail.com>
2025-08-29 22:34:12 +0700

    Initial Commit

---
+++ gleeming/gleemingApp.swift
@@ -0,0 +1,17 @@
+//
+//  gleemingApp.swift
+//  gleeming
+//
+//  Created by Ervan Roni Fahrudi on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct gleemingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gleemingTests/gleemingTests.swift
@@ -0,0 +1,17 @@
+//
+//  gleemingTests.swift
+//  gleemingTests
+//
+//  Created by Ervan Roni Fahrudi on 29/08/25.
+//
+
+import Testing
+@testable import gleeming
+
+struct gleemingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
